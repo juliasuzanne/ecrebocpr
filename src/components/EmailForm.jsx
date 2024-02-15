b4e2407f52cadf2900461d6da92d23f1b1129529
Julia Grimes <julia.s.grimes@gmail.com>
2024-02-15 11:47:10 -0500

    Adjust address in the case that it could be this simple issue

--- src/components/EmailForm.jsx
+++ src/components/EmailForm.jsx
@@ -9,7 +9,7 @@ export function EmailForm() {
     const params = new FormData(event.target);
     setErrors([]);
     axios
-      .post("https://ecrebocpremail.fly.dev/contact_form", params)
+      .post("http://ecrebocpremail.fly.dev/contact_form", params)
       .then((response) => {
         console.log(response.data);
         event.target.reset();
