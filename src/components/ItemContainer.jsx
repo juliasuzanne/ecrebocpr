bd710190da0838ce7cfe1a20afea2c1d4cd28f24
Julia Grimes <julia.s.grimes@gmail.com>
2024-03-07 04:32:38 -0500

    Change responsive strategy

--- src/components/ItemContainer.jsx
+++ src/components/ItemContainer.jsx
@@ -6,27 +6,31 @@ export function ItemContainer(props) {
       <hr></hr>
       <div className="engageitemcontainer">
         <input className="checkbox" type="checkbox" defaultChecked="true" />
-        <img className="itemimage" src={props.image}></img>
-
-        <div id="engageitemdescription">
-          <p className="itemtitle" id="engagehead">
-            {props.title} &nbsp;
-          </p>
-          <p className="itemtitle" id="price">
-            {" "}
-            ${props.newprice}&nbsp;
-            <span className="itemtitle" id="strikethrough">
-              {" "}
-              ${props.oldprice}{" "}
-            </span>
-          </p>
-
-          <p className="itemtitle" id="saving">
-            {" "}
-            Saving ${props.saving} with MMRewards!
-          </p>
+        <div className="col-sm-6 col-md-3 col-lg-3">
+          <img className="col-sm-4 itemimage" src={props.image}></img>
         </div>
-        <p className="col-sm-12 itemtitle" id="points">
+
+        <div className="col-sm-5">
+          <div id="engageitemdescription">
+            <p className="itemtitle" id="engagehead">
+              {props.title} &nbsp;
+            </p>
+            <p className="itemtitle" id="price">
+              {" "}
+              ${props.newprice}&nbsp;
+              <span className="itemtitle" id="strikethrough">
+                {" "}
+                ${props.oldprice}{" "}
+              </span>
+            </p>
+
+            <p className="itemtitle" id="saving">
+              {" "}
+              Saving ${props.saving} with MMRewards!
+            </p>
+          </div>
+        </div>
+        <p className="col-sm-12 col-md-2 itemtitle" id="points">
           {" "}
           2X MMRewards {props.points} Points{" "}
         </p>
