e9a9c4e80339b27dc285b528fa3d748eaba98fb2
Julia Grimes <julia.s.grimes@gmail.com>
2024-03-08 15:19:44 -0500

    Format app store with images

---
+++ src/components/AppStoreLegacy.jsx
@@ -0,0 +1,32 @@
+export function AppStoreLegacy() {
+  <div className="container contentcontainer">
+    <div className="row">
+      <div className="col">
+        <img
+          className="appstorebutton"
+          src="https://res.cloudinary.com/dytb4ayqj/image/upload/v1709701300/mmlogobuttonwhite_pt1vad.png"
+        />
+      </div>
+      <div className="col">
+        <div className="appdescriptiontext">
+          <h5 className="appstoretitle" id="mmtitle">
+            Modern Markets <button className="ratingbutton"> 4+ </button>
+          </h5>
+
+          <p className="appstorelink">Modern Markets Services, Inc.</p>
+          <p className="ranking">#121 in Shopping</p>
+          {/* <div className="rating"> */}
+          {/* <div className="inline-list__item"> */}
+          <img
+            className="starrating"
+            src="https://res.cloudinary.com/dytb4ayqj/image/upload/v1709688241/Screen_Shot_2024-03-05_at_8.23.16_PM_ocyssm.png"
+          />
+          {/* </div> */}
+
+          <p className="free">Free</p>
+          {/* </div> */}
+        </div>
+      </div>
+    </div>
+  </div>;
+}
--- src/components/AppStoreMimic.jsx
+++ src/components/AppStoreMimic.jsx
@@ -17,35 +17,13 @@ export function AppStoreMimic() {
             <p className="banner">This app is available on the App Store for iPhone and iPad.</p>
           </div>
 
-          <div className="container contentcontainer">
-            <div className="row">
-              <div className="col">
-                <img
-                  className="appstorebutton"
-                  src="https://res.cloudinary.com/dytb4ayqj/image/upload/v1709701300/mmlogobuttonwhite_pt1vad.png"
-                />
-              </div>
-              <div className="col">
-                <div className="appdescriptiontext">
-                  <h5 className="appstoretitle" id="mmtitle">
-                    Modern Markets <button className="ratingbutton"> 4+ </button>
-                  </h5>
-
-                  <p className="appstorelink">Modern Markets Services, Inc.</p>
-                  <p className="ranking">#121 in Shopping</p>
-                  {/* <div className="rating"> */}
-                  {/* <div className="inline-list__item"> */}
-                  <img
-                    className="starrating"
-                    src="https://res.cloudinary.com/dytb4ayqj/image/upload/v1709688241/Screen_Shot_2024-03-05_at_8.23.16_PM_ocyssm.png"
-                  />
-                  {/* </div> */}
-
-                  <p className="free">Free</p>
-                  {/* </div> */}
-                </div>
-              </div>
-            </div>
+          <div className="appstorepanel"></div>
+          <hr className="appstorebannerhr"></hr>
+          <div className="appstorebanner">
+            <img
+              className="appstoreinfo"
+              src="https://res.cloudinary.com/dytb4ayqj/image/upload/v1709927584/appmimic-03_pfgbgy.png"
+            ></img>
           </div>
 
           <hr></hr>
