634dd093448e5b520fa988566be0f74defee0e66
Julia Grimes <julia.s.grimes@gmail.com>
2024-03-11 15:44:11 -0400

    Adjust space between animation on mobile

--- src/components/HomePageDescription.jsx
+++ src/components/HomePageDescription.jsx
@@ -2,7 +2,10 @@ export function HomePageDescription() {
   return (
     <div className="description " id="ecrebocpr">
       <h1 className="descriptionhead1">Introducing EcreboCPR</h1>{" "}
-      <span className="descriptionhead2">Clickable Paper Receipts™</span>
+      <span className="descriptionhead2">
+        Clickable Paper Receipts
+        <span id="tm">™</span>
+      </span>
       <br></br>
       <p className="descriptiontext">
         Instantly turn humble paper receipts into powerful digital marketing by adding personalized QR codes.
